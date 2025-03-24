47791b22243e39971c8bc05b01f0401871ca9f7f
hereticpride <allensmalls32@gmail.com>
2025-03-23 22:00:06 -0500

    It's alive! Bookbot lives!

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import sys
 from stats import count_words
 from stats import count_characters
 from stats import sorted_list
 
 def main():
-    book = "./books/frankenstein.txt"
-    get_book = get_book_text(book)
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    get_book = get_book_text(sys.argv[1])
     num_words = count_words(get_book)
     num_chars = count_characters(get_book)
     sort_chars = sorted_list(num_chars)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book}...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
